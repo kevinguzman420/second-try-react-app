6146b06d3dcf7531cb7917af39411137f95567f7
KevinGuzman <guzmankevin420@gmail.com>
2020-10-03 16:11:15 -0600

    Updates

---
+++ precache-manifest.e4fd0492731d5bfd4ebebfd1e1102f9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc6e4dfe4a8c9b0927202d14f72cde6",
+    "url": "/second-try-react-app/index.html"
+  },
+  {
+    "revision": "32b8ba41d206511979c4",
+    "url": "/second-try-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "234b4260582a9f6d64cc",
+    "url": "/second-try-react-app/static/js/2.57b20bc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/second-try-react-app/static/js/2.57b20bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b8ba41d206511979c4",
+    "url": "/second-try-react-app/static/js/main.5cd4bef0.chunk.js"
+  },
+  {
+    "revision": "7385cb0152c7d8715d62",
+    "url": "/second-try-react-app/static/js/runtime-main.9b8367ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/second-try-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);

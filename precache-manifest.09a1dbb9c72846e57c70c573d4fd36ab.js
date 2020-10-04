cdc8d0a48ec91e934db47b85183052edcacd1c3e
KevinGuzman <guzmankevin420@gmail.com>
2020-10-04 15:43:50 -0600

    Updates

---
+++ precache-manifest.09a1dbb9c72846e57c70c573d4fd36ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3306e9f1a9210d3cd017cb781ee61a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d38a448883743264fdee",
+    "url": "/static/css/main.35085826.chunk.css"
+  },
+  {
+    "revision": "438424bf278fb55b9d55",
+    "url": "/static/js/2.ec703da0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ec703da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38a448883743264fdee",
+    "url": "/static/js/main.79ee3648.chunk.js"
+  },
+  {
+    "revision": "ffe148ba9c987666fc58",
+    "url": "/static/js/runtime-main.451dc41f.js"
+  },
+  {
+    "revision": "361b2b35f1700753b305413fe7e4b568",
+    "url": "/static/media/PexelsVideos.361b2b35.mp4"
+  },
+  {
+    "revision": "62233332f5745f48c69601d61c002621",
+    "url": "/static/media/camera.62233332.jpg"
+  },
+  {
+    "revision": "2480cce6188e3f8ae1d9a2e84c2c9cdb",
+    "url": "/static/media/camera2.2480cce6.jpg"
+  },
+  {
+    "revision": "8b9d048be4361c225be9cd5796c1eeb3",
+    "url": "/static/media/camera3.8b9d048b.jpg"
+  },
+  {
+    "revision": "56611ebbb537dc7a98e2de3cee734acc",
+    "url": "/static/media/paper-plane-solid.56611ebb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/second-try-react-app/precache-manifest.70d43ee48228ec7b5c9cf76c9744f922.js"
+  "/precache-manifest.09a1dbb9c72846e57c70c573d4fd36ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/second-try-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
